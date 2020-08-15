b20c31462613e2f05b74a8607c35a231ad56bab2
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-15 21:17:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-function batteries(arr) {
-  let totalBatteries = arr.reduce(reducer, 0);
-  return totalBatteries;
-}
-console.log(batteries(batteryBatches));
+const totalBatteries = batteryBatches.reduce(reducer, 0);
+console.log(totalBatteries);
+

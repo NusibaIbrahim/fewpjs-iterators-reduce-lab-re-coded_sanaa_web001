4ecb022de4dfad66cb8797a6615ab4d9b2c4e56c
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-15 21:09:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 // Code your solution here
 
 function batteries(arr) {
-  let totalBatteries = arr.reduce(
+  var totalBatteries = arr.reduce(
     (accumulator, currentValue) => accumulator + currentValue,
     0
   );
